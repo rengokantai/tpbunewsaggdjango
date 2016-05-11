5dfe58bac8355d483819def9a085727ee4e7a5e9
Hernan Y.Ke <yuriqiao@gmail.com>
2016-05-11 16:43:18 -0400

    adding __str__ to represent model name

--- news/models.py
+++ news/models.py
@@ -6,6 +6,8 @@ class Feed(models.Model):
     title= models.CharField(max_length=200)
     url = models.URLField()
     is_active= models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
 
 class Article(models.Model):
     feed = models.ForeignKey(Feed)
@@ -13,3 +15,5 @@ class Article(models.Model):
     url = models.URLField()
     description = models.TextField()
     publication_date = models.DateTimeField()
+    def __str__(self):
+        return self.title
