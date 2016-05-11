bad4faf5ea39d44c324b00f5263715772eb9c4fc
Hernan Y.Ke <yuriqiao@gmail.com>
2016-05-11 14:53:22 -0400

    Add form

--- news/views.py
+++ news/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
-from .models import Article
+from django.shortcuts import render,redirect
+from .models import Article,Feed
+from .forms import FeedForm
 
 # Create your views here.
 def articles_list(request):
     articles = Article.objects.all()
     return render(request,'news/articles_list.html',{'articles':articles})
+
+def feeds_list(request):
+    feeds = Feed.objects.all()
+    return render(request,'news/feeds_list.html',{'feeds':feeds})
+
+def new_feed(request):
+    if request.method=="POST":
+        form = FeedForm(request.POST)
+        if form.is_valid():
+            feed = form.save(commit=False)
+            feed.title = ""
+            feed.save()
+            return redirect('news.views.feeds_list')
+    else:
+        form = FeedForm()
+    return render(request,'news/new_feed.html',{'form':form})
