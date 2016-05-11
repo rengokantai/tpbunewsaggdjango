a61c570fd0006a477023946fb9f1c6ee46cbc1b0
Hernan Y.Ke <yuriqiao@gmail.com>
2016-05-11 12:39:02 -0400

    Initial

---
+++ news/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Feed(models.Model):
+    title= models.CharField(max_length=200)
+    url = models.URLField()
+    is_active= models.BooleanField(default=False)
+
+class Article(models.Model):
+    feed = models.ForeignKey(Feed)
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    description = models.TextField()
+    publication_date = models.DateTimeField()
